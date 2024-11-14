9e05d10f784f4d19e51641a844760015c2587f02
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2024-11-14 13:07:24 +0100

    feat: Add library template for Tuist project generation

---
+++ Tuist/Templates/library/library.swift
@@ -0,0 +1,25 @@
+import ProjectDescription
+
+let nameAttribute: Template.Attribute = .required("name")
+
+let template = Template(
+  description: "Creates a library module with essential components",
+  attributes: [
+    nameAttribute,
+    .optional("include_tests", default: true)
+  ],
+  items: [
+    // Core files
+    .file(path: "Project.swift", templatePath: "project.stencil"),
+    .file(path: "Sources/\(nameAttribute)Client.swift", templatePath: "client.stencil"),
+    .file(path: "Sources/\(nameAttribute)Feature.swift", templatePath: "feature.stencil"),
+    .file(path: "Sources/\(nameAttribute)Models.swift", templatePath: "models.stencil"),
+
+    // Tests (conditional)
+    .file(
+      path: "Tests/\(nameAttribute)Tests.swift",
+      templatePath: "tests.stencil",
+      condition: .attributeMatch("include_tests", true)
+    )
+  ]
+)
